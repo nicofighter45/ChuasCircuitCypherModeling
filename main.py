ec82f0ac32b4fa5f7ee00905fb79503d0e957386
nicofighter45 <nicolas.fagot@student-cs.fr>
2025-01-17 16:14:47 +0100

    Automatic libraries download

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from app.src.application import App
+import os
+import sys
+import subprocess
 
-APP = None
+
+def install_packages():
+    requirements_file = "requirements.txt"
+    if not os.path.exists(requirements_file):
+        print(f"{requirements_file} not found.")
+        sys.exit(1)
+    try:
+        with open(requirements_file, "r") as f:
+            required_packages = [line.strip() for line in f if line.strip() and not line.startswith("#")]
+        for package in required_packages:
+            package_name = package.split("~")[0].split("=")[0]
+            try:
+                __import__(package_name)
+            except ImportError:
+                print(f"{package_name} is not installed. Installing...")
+                subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    except Exception as e:
+        print(f"Error installing packages: {e}")
+        sys.exit(1)
 
 def launch_app():
-    global APP
-    APP = App("Chaotic Cryptography Simulation")
+    from app.src.application import App
+    app = App("Chaotic Cryptography Simulation")
 
 if __name__ == "__main__":
     print("\n\n\nRunning Chaotic Cryptography Simulation\n\n-----------------------------\n\nCreated by Hugo, Nicolas, Théodore\n\n")
-
+    install_packages()
+    print("\n\nLibraries installed, launching app\n\n")
     # Window setup
     launch_app()
